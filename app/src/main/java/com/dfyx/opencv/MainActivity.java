464a7d5f299006b8855d48b158b0755d25da507a
fn-xiaobo <771672812@qq.com>
2019-08-07 17:48:29 +0800

    更新了开发日志

--- app/src/main/java/com/dfyx/opencv/MainActivity.java
+++ app/src/main/java/com/dfyx/opencv/MainActivity.java
@@ -2,12 +2,18 @@ package com.dfyx.opencv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
