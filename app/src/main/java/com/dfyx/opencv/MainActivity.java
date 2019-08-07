791c320b1b48fc0604cb61c42e1e7daf59e2af2e
fn-xiaobo <771672812@qq.com>
2019-08-07 18:04:55 +0800

    集成了 openCV

--- app/src/main/java/com/dfyx/opencv/MainActivity.java
+++ app/src/main/java/com/dfyx/opencv/MainActivity.java
@@ -1,10 +1,35 @@
 package com.dfyx.opencv;
 
+import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+    private CameraBridgeViewBase openCvCameraView;
+
+    private static final String TAG = "OpencvActivity";
+    private CascadeClassifier cascadeClassifier = null; //级联分类器
+    private Mat mRgba; //图像容器
+    private Mat mGray;
+    private int absoluteFaceSize = 0;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +37,87 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
 
+        handler = new Handler();
 
+        openCvCameraView = (CameraBridgeViewBase) findViewById(R.id.javaCameraView);
+        openCvCameraView.setCameraIndex(0); //摄像头索引        -1/0：后置双摄     1：前置
+        openCvCameraView.enableFpsMeter(); //显示FPS
+        openCvCameraView.setCvCameraViewListener(this);
+
+    }
+
+    private void initializeOpenCVDependencies() {
+        try {
+            InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface_improved); //OpenCV的人脸模型文件： lbpcascade_frontalface_improved
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface_improved.xml");
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+            // 加载cascadeClassifier
+            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+        } catch (Exception e) {
+            Log.e(TAG, "Error loading cascade", e);
+        }
+        // 显示
+        openCvCameraView.enableView();
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(TAG, "OpenCV init error");
+        }
+        initializeOpenCVDependencies();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat();
+        mGray = new Mat();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+        mGray.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba(); //RGBA
+        mGray = inputFrame.gray(); //单通道灰度图
+
+        if (absoluteFaceSize == 0) {
+            int height = mGray.rows();
+            if (Math.round(height * 0.2f) > 0) {
+                absoluteFaceSize = Math.round(height * 0.2f);
+            }
+        }
+
+        //解决  前置摄像头旋转显示问题
+        //Core.flip(mRgba, mRgba, 1); //旋转
+        //Core.flip(mGray, mGray, 1);
+
+        //检测并显示
+        MatOfRect faces = new MatOfRect();
+        if (cascadeClassifier != null) {
+            cascadeClassifier.detectMultiScale(mGray, faces, 1.1, 2, 2, new Size(absoluteFaceSize, absoluteFaceSize), new Size());
+        }
+        Rect[] facesArray = faces.toArray();
+        if (facesArray.length > 0){
+            for (int i = 0; i < facesArray.length; i++) {    //用框标记
+                Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);
+            }
+        }
+        return mRgba;
     }
 
 
